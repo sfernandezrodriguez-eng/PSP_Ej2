379dc83b53d87574751142e2659c0619a1562024
Sergio <sfernandezrodriguez@danielcastelao.org>
2026-09-24 14:36:31 +0200

    Fixing all the system of Interfaz and adding caso2 and part of caso 4

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 
@@ -9,27 +10,39 @@ public class Interfaz {
         String numero = "";
         do {
             System.out.println("\n--- Detector de primos ---");
-            System.out.println("¿Qué nivel quieres usar? (1, 2, 3 o 4) (o 'salir' para terminar):");
+            System.out.println("¿Qué nivel quieres usar? (1, 2, 3 o 4):");
             opcion = new Scanner(System.in).next();
-            System.out.println("Introduce un número:");
-            numero = new Scanner(System.in).next();
             switch (opcion) {
-                case "1" -> {
-                    System.out.println(l.Caso1(numero));
-
+                    case "1" -> {
+                        while (true) {
+                            System.out.println("Introduce un número:");
+                            numero = new Scanner(System.in).next();
+                            if ("salir".equalsIgnoreCase(numero)) {
+                                System.out.println("Saliendo del programa");
+                                break;}
+                            l.Caso1(numero);
+                        }
+                        System.out.println("Saliendo del programa");
+                    }
+                    case "2" -> {
+                        while (true) {
+                            System.out.println("Introduce un número:");
+                            numero = new Scanner(System.in).next();
+                            if ("salir".equalsIgnoreCase(numero)) {
+                                System.out.println("Saliendo del programa");
+                                break;}
+                            l.Caso2(numero);
+                        }
+                        System.out.println("Saliendo del programa");
+                    }
+                    case "3" -> {
+                        //* l.Caso3(numero);
+                    }
+                    case "4" -> {
+                        //* l.Caso4(numero);
+                    }
                 }
-                case "2" -> {
-                    l.Caso2(numero);
-
-                }
-                case "3" -> {
-                    l.Caso3(numero);
-                }
-                case "4" -> {
-                    l.Caso4(numero);
-                }
-                case "salir" -> System.out.println("Chao hasta luego....");
-            }
-        } while (!opcion.equals("salir"));
+        } while (!Objects.equals(opcion, "0"));
+        System.out.println("Saliendo de la aplicación");
     }
 }
--- src/Lanzador.java
+++ src/Lanzador.java
@@ -5,33 +5,79 @@ import java.io.InputStreamReader;
 public class Lanzador {
 
 
-
-
-
-
-
-
-
-    public String Caso1(String numero) {
-        ProcessBuilder factorizar = new ProcessBuilder("factor",numero);
-
+    public void Caso1(String numero) {
         try {
-            Process proceso = factorizar.start();
-            BufferedReader lector = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
-            String resultado = lector.readLine();
-            return resultado;
-        } catch (IOException e) {
-            return "Algo falla 1: " + e.getMessage();
+            int prueba = Integer.parseInt(numero);
+            //*ProcessBuilder factorizar = new ProcessBuilder("factor",numero).start() No necesita Process proceso;
+            ProcessBuilder factorizar = new ProcessBuilder("factor", numero);
+
+            try {
+                Process proceso = factorizar.start();
+                BufferedReader lector = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+                String resultado = lector.readLine();
+                System.out.println(resultado);
+                System.out.println("Operación completada. Código de salida: 0");
+            } catch (IOException e) {
+                System.out.println("Algo falla 1: " + e.getMessage());
+                System.out.println("Operación completada. Código de salida: 1");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Factor: " + numero + " is not a valid positive integer");
+            System.out.println("Operación completada. Código de salida: 1");
         }
     }
 
 
-    public void Caso2(String numero){}
+    public void Caso2(String numero) {
+        try {
+            int prueba = Integer.parseInt(numero);
+            //*ProcessBuilder factorizar = new ProcessBuilder("factor",numero).start() No necesita Process proceso;
+            ProcessBuilder factorizar = new ProcessBuilder("factor", numero);
+
+            try {
+                Process proceso = factorizar.start();
+                BufferedReader lector = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+                String resultado = "[OK] " + lector.readLine();
+                System.out.println(resultado);
+                System.out.println("Operación completada. Código de salida: 0");
+            } catch (IOException e) {
+                System.out.println("Algo falla 1: " + e.getMessage());
+                System.out.println("Operación completada. Código de salida: 1");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] factor: " + numero + " is not a valid positive integer");
+            System.out.println("Operación completada. Código de salida: 1");
+        }
+    }
 
 
-    public void Caso3(String numero){}
+    /**
+     * public String Caso3(String numero){}
+     **/
 
+    public void Caso4(String numero) {
+        try {
+            int prueba = Integer.parseInt(numero);
+            //*ProcessBuilder factorizar = new ProcessBuilder("factor",numero).start() No necesita Process proceso;
+            ProcessBuilder factorizar = new ProcessBuilder("factor", numero);
 
-    public void Caso4(String numero){}
-
+            try {
+                Process proceso = factorizar.start();
+                BufferedReader lector = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+                String partido = lector.readLine();
+                String partes = partido.split(":");
+                if (partes[0].length== partes[1].length+1)){
+                    String resultado = "[OK] " + lector.readLine();
+                    System.out.println(resultado);
+                    System.out.println("Operación completada. Código de salida: 0");
+                }
+            } catch (IOException e) {
+                System.out.println("Algo falla 1: " + e.getMessage());
+                System.out.println("Operación completada. Código de salida: 1");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] factor: " + numero + " is not a valid positive integer");
+            System.out.println("Operación completada. Código de salida: 1");
+        }
+    }
 }
